1248b928830358328cf2cf6674625463378eb473
ooooweeeee <kirby18994929493@icloud.com>
2021-10-10 12:42:20 +0800

    添加antdv

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="app">This is App</div>
+  <div class="app">
+    <a-button type="primary">Primary Button</a-button>
+    <a-input v-model:value="value" placeholder="Basic usage" />
+  </div>
 </template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { Button, Input } from 'ant-design-vue'
+
+export default defineComponent({
+  setup() {
+    const value = ref('')
+    return {
+      value
+    }
+  },
+  components: {
+    [Button.name]: Button,
+    [Input.name]: Input
+  }
+})
+</script>
 
 <style lang="less">
 .app {
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
 
 module.exports = {
   module: {
     rules: [
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
       {
         test: /\.vue$/i,
         exclude: /node_modules/,
@@ -11,12 +17,20 @@ module.exports = {
       },
       {
         test: /\.less$/i,
-        exclude: /node_modules/,
+        include: [
+          /node_modules\/ant-design-vue/,
+          path.resolve(__dirname, 'src/')
+        ],
         use: [
           'vue-style-loader',
           'css-loader',
           {
-            loader: 'less-loader'
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true
+              }
+            }
           },
           {
             loader: 'style-resources-loader',
@@ -28,5 +42,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [new VueLoaderPlugin()]
+  plugins: [new VueLoaderPlugin(), new AntdDayjsWebpackPlugin()]
 }
